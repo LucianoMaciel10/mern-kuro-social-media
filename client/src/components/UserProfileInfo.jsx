42795dc345e0de6b1646cc60e9225de6a00f1359
Luciano Joaquin Maciel <luchomaciel9@gmail.com>
2025-11-27 23:33:34 -0300

    some changes for profile page

--- client/src/components/UserProfileInfo.jsx
+++ client/src/components/UserProfileInfo.jsx
@@ -1,5 +1,6 @@
-import { Verified } from "lucide-react";
+import { Calendar, MapPin, PenBox, Verified } from "lucide-react";
 import { useTheme } from "next-themes";
+import moment from "moment";
 
 const UserProfileInfo = ({ user, posts, profileId, setShowEdit }) => {
   const { theme } = useTheme();
@@ -13,7 +14,11 @@ const UserProfileInfo = ({ user, posts, profileId, setShowEdit }) => {
       }`}
     >
       <div className="flex flex-col md:flex-row items-start gap-6">
-        <div className={`w-32 h-32 border-4 shadow-lg absolute -top-16 rounded-full ${theme === 'dark' ? 'border-neutral-700' : 'border-white'}`}>
+        <div
+          className={`w-32 h-32 border-4 shadow-lg absolute -top-16 rounded-full ${
+            theme === "dark" ? "border-neutral-700" : "border-white"
+          }`}
+        >
           <img
             src={user.profile_picture}
             className="absolute rounded-full z-2"
@@ -39,6 +44,87 @@ const UserProfileInfo = ({ user, posts, profileId, setShowEdit }) => {
               >
                 {user.username ? `@${user.username}` : "Add a username"}
               </p>
+            </div>
+            {!profileId && (
+              <button
+                onClick={() => setShowEdit(true)}
+                className={`flex items-center gap-2 border px-4 rounded-lg font-medium transition-all mt-4 md:mt-0 cursor-pointer active:scale-95 ${
+                  theme === "dark"
+                    ? "border-neutral-700 bg-neutral-900 hover:bg-neutral-800/80"
+                    : "border-neutral-300 bg-neutral-50 hover:bg-neutral-200/50"
+                }`}
+              >
+                <PenBox className="w-4 h-4" />
+                Edit
+              </button>
+            )}
+          </div>
+          <p
+            className={`text-sm max-w-md mt-4 ${
+              theme === "dark" ? "text-neutral-400/80" : "text-neutral-500"
+            }`}
+          >
+            {user.bio}
+          </p>
+
+          <div
+            className={`flex flex-wrap items-center gap-x-6 gap-y-2 text-sm mt-4 ${
+              theme === "dark" ? "text-neutral-400/60" : "text-neutral-500/85"
+            }`}
+          >
+            <span className="flex items-center gap-1.5">
+              <MapPin className="w-5 h-5" />
+              {user.location ? user.location : "Add location"}
+            </span>
+            <span className="flex items-center gap-1.5">
+              <Calendar className="w-5 h-5" />
+              Joined{" "}
+              <span
+                className={`font-semibold ${
+                  theme === "dark" && "text-neutral-400/80"
+                }`}
+              >
+                {moment(user.createdAt).fromNow()}
+              </span>
+            </span>
+          </div>
+
+          <div className="flex items-center gap-6 mt-6 border-t border-neutral-200 pt-2">
+            <div>
+              <span
+                className={`sm:text-xl font-bold ${
+                  theme === "dark" ? "text-neutral-200" : "text-neutral-900"
+                }`}
+              >
+                {posts.length}
+              </span>
+              <span className="text-xs sm:text-sm ml-1.5 text-neutral-500">
+                Posts
+              </span>
+            </div>
+            <div>
+              <span
+                className={`sm:text-xl font-bold ${
+                  theme === "dark" ? "text-neutral-200" : "text-neutral-900"
+                }`}
+              >
+                {user.followers.length}
+              </span>
+              <span className="text-xs sm:text-sm text-neutral-500 ml-1.5">
+                Followers
+              </span>
+            </div>
+            <div>
+              <span
+                className={`sm:text-xl font-bold ${
+                  theme === "dark" ? "text-neutral-200" : "text-neutral-900"
+                }`}
+              >
+                {user.following.length}
+              </span>
+              <span className="text-xs sm:text-sm text-neutral-500 ml-1.5">
+                Following
+              </span>
             </div>
           </div>
         </div>
