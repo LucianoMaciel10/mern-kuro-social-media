d6dc4076fe3689814dca85c789bc0773fc23b161
Luciano Joaquin Maciel <luchomaciel9@gmail.com>
2025-12-01 21:05:56 -0300

    initial setup for the backend

---
+++ server/src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    full_name: { type: String, required: true },
+    username: { type: String, required: true, unique: true, lowercase: true },
+    bio: { type: String, default: "Hey there! I am using Kuro." },
+    profile_picture: { type: String, default: "" },
+    cover_photo: { type: String, default: "" },
+    location: { type: String, default: "" },
+    followers: [{ type: String, ref: "User" }],
+    following: [{ type: String, ref: "User" }],
+    isVerified: { type: Boolean, default: false },
+  },
+  { timestamps: true, minimize: false }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./configs/db.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Server is running!"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+connectDB().then(() =>
+  app.listen(PORT, () => console.log("Server is running on port " + PORT))
+);
